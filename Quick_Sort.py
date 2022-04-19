26be9e0eecaf89bac6f7db49cf6d10491e108156
廖祐陞vincent_liao <vincent_liao@tyrafos.local>
2022-04-19 11:34:31 +0800

    update Quick Sort

--- Quick_Sort.py
+++ Quick_Sort.py
@@ -1,31 +1,28 @@
-class Solution:
-    def sortArray(self, nums: list[int]) -> list[int]:
-        self.quicksort(nums, 0, len(nums) - 1)
+import random
+def quicksort(nums, left, right):
+    if left < right:
+        reference = nums[left]
+        l = left
+        r = right
+        while l < r:
+            while nums[l] <= reference and l < right:
+                l += 1
+            while nums[r] >  reference and left < r:
+                r -=1
+            if l < r:
+                nums[r], nums[l] = nums[l], nums[r]
+
+        nums[left], nums[r] = nums[r], nums[left]
+
+        quicksort(nums, left,   r-1)
+        quicksort(nums,  r+1, right)
         return nums
 
-    def quicksort(self, nums, left, right):
-        if left < right:
-            reference = nums[left]
-            l = left
-            r = right
-            while l < r:
-                while nums[l] <= reference and l < right:
-                    l += 1
-                while nums[r] >  reference and left < r:
-                    r -=1
-                if l < r:
-                    nums[r], nums[l] = nums[l], nums[r]
-
-            nums[left], nums[r] = nums[r], nums[left]
-
-            self.quicksort(nums, left,   r-1)
-            self.quicksort(nums,  r+1, right)
-
 def main():
-    solution = Solution()
-    nums = [5,2,3,1]
-    solution.quicksort(nums, 0, len(nums) - 1)
-    print(nums)
+    unsorted_list = [random.randint(0, 100) for _ in range(10)]
+    print("Before Selection Sort\t: ", unsorted_list)
+    quicksort(unsorted_list, 0, len(unsorted_list) - 1)
+    print("After Selevtion Sort\t: ", quicksort(unsorted_list, 0, len(unsorted_list) - 1))
 
 if __name__ == '__main__':
     main()
