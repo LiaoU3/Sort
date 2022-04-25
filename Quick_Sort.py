9aa13c6a50916d01a543ff97c8d70de771a36e27
LiaoU3 <vincent932693@gmail.com>
2022-04-25 10:59:08 +0800

    update Comparison and Quick Sort

--- Quick_Sort.py
+++ Quick_Sort.py
@@ -1,12 +1,8 @@
 import random
-<<<<<<< HEAD
 def quick_sort(nums):
     return _sort(nums, 0, len(nums) - 1)
 
 def _sort(nums, left, right):
-=======
-def quicksort(nums, left, right):
->>>>>>> 717ded4590b997f90347a13315224fb86bacae00
     if left < right:
         reference = nums[left]
         l = left
@@ -21,25 +17,15 @@ def quicksort(nums, left, right):
 
         nums[left], nums[r] = nums[r], nums[left]
 
-<<<<<<< HEAD
         _sort(nums, left,   r-1)
         _sort(nums,  r+1, right)
-=======
-        quicksort(nums, left,   r-1)
-        quicksort(nums,  r+1, right)
->>>>>>> 717ded4590b997f90347a13315224fb86bacae00
         return nums
 
 def main():
     unsorted_list = [random.randint(0, 100) for _ in range(10)]
     print("Before Selection Sort\t: ", unsorted_list)
-<<<<<<< HEAD
     quick_sort(unsorted_list)
     print("After Selevtion Sort\t: ", quick_sort(unsorted_list))
-=======
-    quicksort(unsorted_list, 0, len(unsorted_list) - 1)
-    print("After Selevtion Sort\t: ", quicksort(unsorted_list, 0, len(unsorted_list) - 1))
->>>>>>> 717ded4590b997f90347a13315224fb86bacae00
 
 if __name__ == '__main__':
     main()
